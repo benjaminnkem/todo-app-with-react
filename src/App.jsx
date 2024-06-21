33ac014c6591b4a7af127d2218b6b779f8abc0c0
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-06-21 21:08:28 +0100

    fe

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import Todo from "./components/Todo";
+import { useState } from "react";
+import TodoList from "./components/TodoList";
+import { toast } from "sonner";
 
 const App = () => {
-  return (
-    <div>
-      <h1>To do list in React</h1>
+  const [todos, setTodos] = useState([]);
 
-      <Todo />
+  return (
+    <div className="grid w-full h-screen place-content-center">
+      <div className="space-y-4 min-w-96">
+        <div className="p-4 font-bold text-center rounded-md bg-primary">
+          <h1>React ToDo</h1>
+        </div>
+
+        <TodoList todos={todos} />
+      </div>
     </div>
   );
 };
