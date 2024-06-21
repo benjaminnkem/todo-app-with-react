adfbec19c6778efac9641aad9c845d8d5c24ebf0
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-06-21 13:00:23 +0100

    Todo List with react tutorial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Todo from "./components/Todo";
+
+const App = () => {
+  return (
+    <div>
+      <h1>To do list in React</h1>
+
+      <Todo />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,9 @@
+const Todo = () => {
+  return (
+    <div>
+      <h2>To do component</h2>
+    </div>
+  );
+};
+
+export default Todo;
