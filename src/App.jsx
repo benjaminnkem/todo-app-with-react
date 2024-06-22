abbca9220c600947a929c7c794f01fb4d8875adc
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-06-22 01:18:04 +0100

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
 import TodoList from "./components/TodoList";
-import { toast } from "sonner";
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-
   return (
     <div className="grid w-full h-screen place-content-center">
       <div className="space-y-4 min-w-96">
@@ -12,7 +8,7 @@ const App = () => {
           <h1>React ToDo</h1>
         </div>
 
-        <TodoList todos={todos} />
+        <TodoList />
       </div>
     </div>
   );
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,64 +1,9 @@
-/**
- * Properties
- * - id
- * - task
- * - complete
- */
-
-import { useState } from "react";
-import { MdCancel } from "react-icons/md";
-import Todo from "./Todo";
-import { FaPlus } from "react-icons/fa6";
-import { toast } from "sonner";
-
-const TodoList = ({ todos, addTodo, removeTodo, completeTodo }) => {
-  const [addNewOn, setAddNewOn] = useState(false);
-  const [taskInput, setTaskInput] = useState("");
-
+const TodoList = () => {
   return (
-    <div className="relative p-5 bg-white border rounded-md">
-      {todos && todos.length > 0 ? (
-        <div className="divide-y max-h-[20rem] overflow-y-auto">
-          {todos.map((todo, id) => (
-            <Todo key={id} removeTodo={removeTodo} todo={todo} completeTodo={completeTodo} />
-          ))}
-        </div>
-      ) : (
-        <div className="space-y-3">
-          <p className="text-center pb-3 text-[#9ea8c4]">No todos</p>
-        </div>
-      )}
-
-      {addNewOn && (
-        <div className="flex items-center gap-2">
-          <input
-            type="text"
-            className="flex-grow w-full p-2 text-gray-500 border-b outline-none"
-            placeholder="Enter task"
-            value={taskInput}
-            onChange={(e) => setTaskInput(e.target.value)}
-          />
-
-          <div className="text-red-400 cursor-pointer size-4" onClick={() => setAddNewOn(false)}>
-            <MdCancel size={20} />
-          </div>
-        </div>
-      )}
-
-      <button className="flex items-center justify-center w-full gap-2 px-4 py-2 mt-3 font-bold rounded-2xl -bottom-6 bg-primary">
-        <FaPlus />
-        <span> {addNewOn ? "Add" : "New task"}</span>
-      </button>
+    <div className="p-5 bg-white border rounded-md">
+      <button></button>
     </div>
   );
-};
-
-TodoList.propTypes = {
-  todos: [],
-  setTodos: Function,
-  addTodo: Function,
-  removeTodo: Function,
-  completeTodo: Function,
 };
 
 export default TodoList;
